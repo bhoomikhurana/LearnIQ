a6fe7c692b0e495a81314b969665d6eea83ec044
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2024-03-05 02:04:37 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
+import Login from './components/Auth/Login';
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/courses" element={<Courses />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
       <Footer />
     </Router>
