3955f9269a11a0a0dd44ea5ff661266ab95f97c6
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2024-04-26 01:55:27 +0530

    add lecture form

--- frontend/src/components/Admin/AdminCourses/CourseModal.jsx
+++ frontend/src/components/Admin/AdminCourses/CourseModal.jsx
@@ -3,17 +3,21 @@ import {
   Button,
   Grid,
   Heading,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { RiDeleteBin7Fill } from 'react-icons/ri';
+import { fileUploadCss } from '../../Auth/Register';
 
 const CourseModal = ({
   isOpen,
@@ -22,10 +26,35 @@ const CourseModal = ({
   deleteButtonHandler,
   addLectureHandler,
   courseTitle,
-  lecture = [],
+  lectures = [1, 2, 3, 4, 5, 6, 7, 8],
 }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [video, setVideo] = useState('');
+  const [videoPrev, setVideoPrev] = useState('');
+  const changeVideoHandler = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setVideoPrev(reader.result);
+      setVideo(file);
+    };
+  };
+  const handleClose = () => {
+    setTitle('');
+    setDescription('');
+    setVideoPrev('');
+    setVideo('');
+    onClose();
+  };
   return (
-    <Modal isOpen={isOpen} size={'full'}>
+    <Modal
+      isOpen={isOpen}
+      size={'full'}
+      onClose={handleClose}
+      scrollBehavior="outside"
+    >
       <ModalOverlay>
         <ModalContent>
           <ModalHeader>{courseTitle}</ModalHeader>
@@ -38,17 +67,74 @@ const CourseModal = ({
                   <Heading size={'sm'} opacity={0.4}>{`#${id}`}</Heading>
                 </Box>
                 <Heading size={'lg'}>Lectures</Heading>
-                <VideoCard
-                  title={'React Intro'}
-                  description={'This is intro vid'}
-                  num={1}
-                  lectureId={'dcefvefvcs'}
-                  courseId={id}
-                  deleteButtonHandler={deleteButtonHandler}
-                />
+                {lectures.map((item, index) => (
+                  <VideoCard
+                    key={index}
+                    title={'React Intro'}
+                    description={'This is intro vid'}
+                    num={index + 1}
+                    lectureId="defewferfrfe"
+                    courseId={id}
+                    deleteButtonHandler={deleteButtonHandler}
+                  />
+                ))}
+              </Box>
+              <Box>
+                <form
+                  onSubmit={e => {
+                    addLectureHandler(e, id, title, description, video);
+                  }}
+                >
+                  <VStack spacing={'4'}>
+                    <Heading
+                      textTransform={'uppercase'}
+                      size={'md'}
+                      children="Add Lecture"
+                    />
+                    <Input
+                      focusBorderColor="purple.300"
+                      placeholder="Title"
+                      value={title}
+                      onChange={e => setTitle(e.target.value)}
+                    />
+                    <Input
+                      focusBorderColor="purple.300"
+                      placeholder="Description"
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}
+                    />
+                    <Input
+                      focusBorderColor="purple.300"
+                      type="file"
+                      required
+                      onChange={changeVideoHandler}
+                      accept="video/mp4"
+                      placeholder="Choose File"
+                      css={{
+                        '&::file-selector-button': {
+                          ...fileUploadCss,
+                          color: 'purple',
+                        },
+                      }}
+                    />
+                    {videoPrev && (
+                      <video
+                        controlsList="nodownload"
+                        controls
+                        src={videoPrev}
+                      ></video>
+                    )}
+                    <Button colorScheme="purple" w={'full'} type="submit">
+                      Upload
+                    </Button>
+                  </VStack>
+                </form>
               </Box>
             </Grid>
           </ModalBody>
+          <ModalFooter>
+            <Button onClick={handleClose}>Close</Button>
+          </ModalFooter>
         </ModalContent>
       </ModalOverlay>
     </Modal>
