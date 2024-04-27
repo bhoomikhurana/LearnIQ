edf04315844a90855ece021d4137c45288f8f3ef
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2024-04-28 03:37:06 +0530

    courses api

--- learniqserver/app.js
+++ learniqserver/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
+
 const app = express();
 
 config({
   path: "./config/config.env",
 });
-import course from "./routes/courseRoutes.js";
-app.use("/api/v1", course);
+import courseRoutes from "./routes/courseRoutes.js";
+app.use("/api/v1", courseRoutes);
 export default app;
--- learniqserver/controllers/courseController.js
+++ learniqserver/controllers/courseController.js
@@ -1,3 +1,18 @@
-export const getAllCourses = (req, res, next) => {
-  res.send("Working");
+import { Course } from "../models/Course.js";
+
+export const getAllCourses = async (req, res, next) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).send({
+      success: true,
+      courses,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updating Order",
+      error,
+    });
+  }
 };
