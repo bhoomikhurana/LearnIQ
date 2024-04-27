0870009c53c5db7e4e5fffed32c6406b1f6a1875
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2024-04-28 01:14:13 +0530

    course and user model

--- learniqserver/app.js
+++ learniqserver/app.js
@@ -5,4 +5,6 @@ const app = express();
 config({
   path: "./config/config.env",
 });
+import course from "./routes/courseRoutes.js";
+app.use("/api/v1", course);
 export default app;
---
+++ learniqserver/models/User.js
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema({});
+
+export const User = mongoose.model("User", schema);
